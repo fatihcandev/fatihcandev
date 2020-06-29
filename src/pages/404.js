adb60fd7182834f93b3ad96e8e18af08fb48b0d7
Fatih <fatihcan.social@gmail.com>
2020-06-29 19:48:46 +0300

    add 404 page and styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { Link } from 'gatsby';
+import SEO from '../components/seo';
+import NotFoundImage from '../components/not-found-img';
+import TextBody from '../components/text-body';
+
+import '../styles/404.css';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <>
+        <SEO title="404: Not found" />
+        <div className="wrapper">
+            <NotFoundImage />
+            <TextBody>
+                Oops! You've got lost.{' '}
+                <Link to="/" title="Go back home">
+                    <span className="home">Go back home</span>
+                </Link>
+            </TextBody>
+        </div>
+    </>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
