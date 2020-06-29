436698cf4ea7915d914bd135db262e4c895c11cd
Fatih <fatihcan.social@gmail.com>
2020-06-29 11:41:14 +0300

    add index page

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+    <Layout>
+        <SEO />
+    </Layout>
+);
+
+export default IndexPage;
