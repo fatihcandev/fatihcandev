49b6c2e0c56af7659dc9e9442183e0543540b77f
Fatih <fatihcan.social@gmail.com>
2020-06-29 11:39:46 +0300

    add siteMetadata for seo

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Fatih Can | Front-end Developer',
+        description:
+            "Hi! I'm Fatih. I'm a front-end developer based in İzmir, Turkey.",
+        url: 'https://fatihcan.dev',
+        image: `${__dirname}/src/images/meta-image.jpg`,
+        twitterUsername: '@fatihcandev',
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+    ],
+};
