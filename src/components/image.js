d9aa3dd310089ba2f3fe1e3d9a136bd7db916aba
Fatih <fatihcan.social@gmail.com>
2020-06-29 19:52:06 +0300

    add gatsby image component

--- src/components/image.js
+++ src/components/image.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Img from 'gatsby-image';
 
-const Image = ({ img, alt }) => {
-    return <Img fluid={img} alt={alt} />;
+const Image = ({ img, alt, ...props }) => {
+    return <Img fluid={img} alt={alt} {...props} />;
 };
 
 export default Image;
