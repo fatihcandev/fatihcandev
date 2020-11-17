53109fd3e844d1f4f5869b3fe21dea6edfae6bd8
Fatih <54099904+fatihcandev@users.noreply.github.com>
2020-11-17 12:39:50 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,7 +26,7 @@ const About = () => {
                         born in Ankara, Turkey <Emoji label="baby">👶</Emoji>{' '}
                         lived there for 6 years and my family moved to İzmir in
                         2003. Now, it's been nearly 17 years since we came here.
-                        <br />I was always interested in computers but not in a
+                        <br /><br />I've always been interested in computers but not in a
                         good way. I would always play video games and that's how
                         I got into a relatively bad high school but that place
                         was where I met with programming{' '}
@@ -35,7 +35,7 @@ const About = () => {
                         but if I brainstorm on it a while, I could come up with
                         something. So, there was hope until I went to college
                         and study in an education department.
-                        <br /> The college kept me busy and careless enough to
+                        <br /><br /> The college kept me busy and careless enough to
                         not to write a single line of code for years{' '}
                         <Emoji label="man facepalming">🤦‍♂</Emoji> But one day,
                         someone came up to me and asked me if I know how to
@@ -50,7 +50,7 @@ const About = () => {
                         so I decided to be a developer. The path looked hard and
                         long but I didn’t give up (yet{' '}
                         <Emoji label="grinning face with sweat">😅</Emoji>).
-                        <br />I switched between the back-end and front-end area
+                        <br /><br />I switched between the back-end and front-end area
                         but I've decided to be a front-end developer because I
                         love to take a look at something and imagine the columns
                         and row and build it with code. For the last five
@@ -59,7 +59,7 @@ const About = () => {
                         what I was working towards. I've been hired as a
                         front-end developer in July{' '}
                         <Emoji label="partying face">🥳</Emoji> But the learning
-                        journey is not over yet. <br />
+                        journey is not over yet. <br /><br />
                         Well, enough talking about programming. I love watching
                         movies, series and listening to music. My music taste
                         spectrum is really wide. I'll leave my On Repeat list
