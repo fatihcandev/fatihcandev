74601d8d2ccd7b27a685ff2e6d4bc5417f7595f9
fatihcandev <fatihcan.social@gmail.com>
2020-11-22 21:51:39 +0300

    add readme. prevent mobile nav scroll

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -2,13 +2,16 @@ const navbar = document.querySelector(".menu-m");
 const opener = document.getElementById("nav-opener");
 const closer = document.getElementById("nav-closer");
 const navItem = document.getElementById("nav-item");
+const body = document.querySelector("body");
 
 opener.addEventListener("click", () => {
   navbar.className = "menu-m show";
+  body.className = "no-scroll";
 });
 
 closer.addEventListener("click", () => {
   navbar.className = "menu-m";
+  body.className = "";
 });
 
 navItem.addEventListener("click", () => {
