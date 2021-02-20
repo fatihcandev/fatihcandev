932ddfecbfb5597987fcfa537cab46bc527b4b3d
fatihcandev <contact@fatihcan.dev>
2021-02-21 00:41:03 +0300

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Flex, HStack, Link, Text, useMediaQuery } from "@chakra-ui/react"
+
+import ChakraUILogo from "../ChakraUILogo"
+
+const Footer = () => {
+  const [isDesktop] = useMediaQuery("(min-width: 768px)")
+  return (
+    <Flex justify="center" align="center">
+      <HStack>
+        <Text fontSize="lg" color={["white", null, "gray.800"]}>
+          Made with
+        </Text>
+        <Link href="https://chakra-ui.com/" isExternal>
+          <ChakraUILogo textColor={isDesktop ? "#2D3748" : "#fff"} />
+        </Link>
+      </HStack>
+    </Flex>
+  )
+}
+
+export default Footer
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,6 +4,7 @@ import { Box, useDisclosure } from "@chakra-ui/react"
 import SocialLinks from "../SocialLinks"
 import DrawerComponent from "../DrawerComponent"
 import Navbar from "../Navbar"
+import Footer from "../Footer"
 
 interface ILayoutProps {
   children: React.ReactNode
@@ -21,6 +22,9 @@ const Layout = ({ children }: ILayoutProps) => {
       <Navbar onOpenDrawer={onOpenDrawer} />
       <Box display={["none", null, "block"]} pos="fixed" left={4} bottom={4}>
         <SocialLinks />
+      </Box>
+      <Box pos="fixed" left={0} right={0} bottom={4} zIndex={2}>
+        <Footer />
       </Box>
       {children}
       {isDrawerOpen && (
