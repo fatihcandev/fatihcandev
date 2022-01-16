cdb157dc596a785d6c1e039057c678a310d124f3
Fatih <contact@fatihcan.dev>
2022-01-16 23:56:21 +0300

    add superpeer widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
+import Script from 'next/script'
 
 import { globalStyles } from 'styles/globals'
 
@@ -10,7 +11,18 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Fatih Can | Front End Developer</title>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://widgets.superpeer.com/widget.css"
+        />
       </Head>
+      <Script src="https://widgets.superpeer.com/widget.js" />
+      <Script id="superpeerWidget">
+        {`
+              window.addEventListener("load", () => {new Superpeer.Widget({embed:{type:"slide-in"},launcher:{type:"superpeer-logo",options:{}},config:{username:"fatihcandev",serviceSlug:""}})})
+            `}
+      </Script>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
